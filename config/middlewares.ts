edcc3c43a31c513fe7e02df8b8801ad3c6d90105
mariamelsayed12 <me282573@gmail.com>
2025-05-17 21:17:38 +0300

    Add client side

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -14,7 +14,18 @@ export default [
       },
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',               // للـ local dev
+        'https://full-stack-ecommerce-app-shopzy.vercel.app/'       // استبدليها بالدومين الحقيقي
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: '*',
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
